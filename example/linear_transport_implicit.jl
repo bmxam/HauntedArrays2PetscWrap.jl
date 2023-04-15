1cd7130efe634fc0e58817f61895e75b1d338dff
bmxam <maxime.bouyges@gmail.com>
2023-04-15 16:00:50 +0200

    success of implicit with PetscFactorization

--- example/linear_transport_implicit.jl
+++ example/linear_transport_implicit.jl
@@ -1,92 +1,15 @@
-# see https://docs.sciml.ai/LinearSolve/dev/solvers/solvers/#HYPRE.jl
-# for a note on how to use custom LinearSolver and preconditioners
-#
-# or https://docs.sciml.ai/LinearSolve/dev/advanced/developing/
-#
-#
-# and https://docs.sciml.ai/LinearSolve/dev/advanced/custom/
 module LinearTransport
 using MPI
 using MPIUtils
 using HauntedArrays
 using OrdinaryDiffEq
-using DiffEqBase
 using HauntedArrays2PetscWrap
-using LinearSolve
 using LinearAlgebra
-using SciMLBase
 
+# Settings
 const lx = 1.0
 const nx = 4 # on each process
 const c = 1.0
-
-function DiffEqBase.recursive_length(A::HauntedVector)
-    MPI.Allreduce(n_own_rows(A), MPI.SUM, get_comm(A))
-end
-
-function my_linsolve!(
-    A::OrdinaryDiffEq.WOperator{IIP},
-    b,
-    u,
-    p,
-    newA,
-    Pl,
-    Pr,
-    solverdata;
-    verbose = true,
-    kwargs...,
-) where {IIP}
-    @show typeof(A.mass_matrix)
-    @show typeof(A.gamma)
-    @show typeof(A.J)
-    # @show typeof(convert(AbstractMatrix, A.J))
-    @show typeof(A._concrete_form)
-    @show A.transform
-    @show IIP
-    error("toto")
-    A = convert(AbstractMatrix, A)
-    u .= LinearAlgebra.ldiv!(u, A, b)
-    return u
-end
-
-# struct MyLUFactorization <: SciMLBase.AbstractLinearAlgorithm end
-struct MyLUFactorization <: LinearSolve.AbstractFactorization end
-# LinearSolve.needs_concrete_A(::MyLUFactorization) = true
-
-function LinearSolve.init_cacheval(
-    alg::MyLUFactorization,
-    A,
-    b,
-    u,
-    Pl,
-    Pr,
-    maxiters::Int,
-    abstol,
-    reltol,
-    verbose::Bool,
-    assumptions::LinearSolve.OperatorAssumptions,
-)
-    # do nothing for now
-end
-
-# function SciMLBase.solve(cache::LinearCache, alg::MyLUFactorization; kwargs...)
-function SciMLBase.solve(cache::LinearSolve.LinearCache, alg::MyLUFactorization; kwargs...)
-    # Version for LinearSolve.AbstractFactorization :
-    # if cache.isfresh
-    #     fact = do_factorization(alg, cache.A, cache.b, cache.u)
-    #     cache = set_cacheval(cache, fact)
-    # end
-    # y = ldiv!(cache.u, cache.cacheval, cache.b)
-
-    y = ldiv!(cache.u, cache.A, cache.b)
-    # @show typeof(cache.u)
-    # @show typeof(cache.A)
-    # @show typeof(cache.b)
-    # @show typeof(y)
-    # error("debug")
-    SciMLBase.build_linear_solution(alg, y, nothing, cache)
-end
-
 
 MPI.Initialized() || MPI.Init()
 
@@ -117,12 +40,11 @@ end
 
 tspan = (0.0, 2.0)
 prob = ODEProblem(f!, q, tspan, p)
-alg = MyLUFactorization()
 
 always_true(args...) = true
 
 # This callback avoids using `update_ghosts!` in `f`, which
-# can cause troubles when dealing with Dual
+# can cause troubles when dealing with Dual elements
 cb_update = DiscreteCallback(
     always_true,
     integrator -> begin
@@ -132,10 +54,8 @@ cb_update = DiscreteCallback(
 )
 
 @only_root println("running solve...")
-# timestepper = Euler()
-timestepper = ImplicitEuler(linsolve = alg)
+timestepper = ImplicitEuler(linsolve = PetscFactorization())
 sol = solve(prob, timestepper; callback = CallbackSet(cb_update))
-# sol = solve(prob, Euler(); dt = Δx / c)
 q = sol.u[end]
 
 @one_at_a_time @show owned_values(q)
